132d44d53b984dd6bb84c89d32f4ac2d2173c87d
Ejiro Asiuwhu <Ejiroasiuwhu10@gmail.com>
2026-01-29 08:02:11 +0100

    feat(svelte): add Svelte playground with initial setup and configurations

---
+++ playgrounds/svelte/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import assetManager from '../../src/index'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [svelte(), assetManager({
+    watch: false
+  })],
+  server: {
+    open: true
+  }
+})
