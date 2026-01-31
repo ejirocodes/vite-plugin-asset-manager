d2f48e6dd3bfb375e031eaa2ce8f1baf6c78834e
Ejiro Asiuwhu <Ejiroasiuwhu10@gmail.com>
2026-01-31 13:17:34 +0100

    feat(playground): add tanstack start react playground

---
+++ playgrounds/tanstack/src/routes/index.tsx
@@ -0,0 +1,38 @@
+import { createFileRoute } from '@tanstack/react-router'
+import '../App.css'
+import viteLogo from '../logo.svg'
+
+export const Route = createFileRoute('/')({ component: App })
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="/tanstack-circle-logo.png"
+          className="App-logo"
+          alt="TanStack Logo"
+        />
+        <p>
+          Edit <code>src/routes/index.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a
+          className="App-link"
+          href="https://tanstack.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn TanStack
+        </a>
+      </header>
+    </div>
+  )
+}
