4404edb11b8843bacd76c9ab5b56b94f4c0bf7f0
Ejiro Asiuwhu <Ejiroasiuwhu10@gmail.com>
2026-02-10 19:16:59 +0100

    chore: upgrade nuxt playground to Nuxt 4
    - Upgrade nuxt from ^3.17.5 to ^4.3.1
    - Migrate to app/ directory structure (Nuxt 4 convention)
    - Move app.vue to app/app.vue, remove old pages/
    - Add test assets (gif, pdf, svg) and public files
    - Simplify nuxt.config.ts for Nuxt 4

---
+++ playgrounds/nuxt/app/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <img src="~/assets/unused-duplicate-icon.svg" alt="Discover Nuxt" />
+    <img src="/assets/unused-duplicate-icon.svg" alt="Discover Nuxt" />
+    <img src="/favicon.ico" alt="Discover Nuxt" />
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+  </div>
+</template>
